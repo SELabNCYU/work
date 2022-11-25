a671b05bbf5226b6c7098f3682cea93efee46d55
SELabNCYU <119007380+SELabNCYU@users.noreply.github.com>
2022-11-25 19:46:52 +0800

    Update verify.sh

--- data/verify.sh
+++ data/verify.sh
@@ -2,6 +2,7 @@
 set -e
 cat data/graph.txt
 echo --------------------
+git fetch
 git checkout test
 echo --------------------
 git log --graph --abbrev-commit --decorate --format=format:'%C(white)%s%C(reset) %C(dim white)' > ex_graph.txt
