67d2aec94c9f5b2f472f0a5ed60bbef5fe2dee76
SELabNCYU <119007380+SELabNCYU@users.noreply.github.com>
2022-11-25 19:11:45 +0800

    Update verify.sh

--- data/verify.sh
+++ data/verify.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 set -e
 
-ls
-cd work
-ls
 cat data/graph.txt
 git checkout $PROJECT_BRANCH
 git log --graph --abbrev-commit --decorate --format=format:'%C(white)%s%C(reset) %C(dim white)' $PROJECT_BRANCH > ../ex_graph.txt
-cd ..
+
 cat ex_graph.txt
-if [ `diff ex_graph.txt work/data/graph.txt | wc -l` -ne 0 ]; then
+if [ `diff ex_graph.txt data/graph.txt | wc -l` -ne 0 ]; then
 	echo "Your rebase or merge operation is incorrect!"
 	exit 1
 fi
 
-if [ `diff 1A_elements.txt work/data/data.txt | wc -l` -ne 0 ]; then
+if [ `diff 1A_elements.txt data/data.txt | wc -l` -ne 0 ]; then
 	echo "1A_elements.txt content is incorrect!"
 	exit 1
 fi
