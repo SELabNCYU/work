caebf0ea576e84eda4e3286bbaededf9472e8bfc
SELabNCYU <119007380+SELabNCYU@users.noreply.github.com>
2022-11-25 19:28:46 +0800

    Update verify.sh

--- data/verify.sh
+++ data/verify.sh
@@ -6,7 +6,7 @@ git checkout $PROJECT_BRANCH
 #git log --graph --abbrev-commit --decorate --format=format:'%C(white)%s%C(reset) %C(dim white)' $PROJECT_BRANCH > ex_graph.txt
 #echo 123 $PROJECT_BRANCH
 #ls
-git log --graph --abbrev-commit --decorate --format=format:'%C(white)%s%C(reset) %C(dim white) > ex_graph.txt
+git log --graph --abbrev-commit --decorate --format=format:'%C(white)%s%C(reset) %C(dim white)' > ex_graph.txt
 cat ex_graph.txt
 
 if [ `diff ex_graph.txt data/graph.txt | wc -l` -ne 0 ]; then
